89ce8ed1ce970ad836ff945c9b257e7a24768b5d
Ben Nappier <ben2113@columbia.edu>
2013-10-11 20:38:46 -0400

    first commit, added play button

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sokoban::Application.config.secret_key_base = 'caa4feafc7b4cc10e51ff0dbe69a4321ed111ddb82e86e200e0ec6da616a235ea4f021efda811eacc3d7680cac46e24d733979b5d1ccc89d8cd45b5e5aba1b62'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sokoban::Application.config.session_store :cookie_store, key: '_sokoban_session'
